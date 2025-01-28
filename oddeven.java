d43445bcd57e4f2152cda12ab34d4053f7a51738
Nitinkumar-1 <nitinkumar766823@gmail.com>
2025-01-28 15:48:29 +0600

    first commit

---
+++ oddeven.java
@@ -0,0 +1,11 @@
+public class oddeven{
+    public static void main(String[] args){
+    for(int i=1;i<=10;i++){ 
+        if(i%2==0){
+            System.out.println("the number" + i + "is even");
+        }else{
+            System.out.println("the number" + i + "is odd");
+        }
+    }
+}
+}
